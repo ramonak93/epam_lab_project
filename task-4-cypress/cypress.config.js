379dadb347edeb8ac9add9f14c04ce3d663a23be
Siarhei Ramanishkin <siarhei.ramonak@gmail.com>
2026-06-29 20:04:53 +0300

    task-4: update config

--- task-4-cypress/cypress.config.js
+++ task-4-cypress/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+  defaultBrowser: "firefox",
 
   e2e: {
     baseUrl: "https://practicesoftwaretesting.com",

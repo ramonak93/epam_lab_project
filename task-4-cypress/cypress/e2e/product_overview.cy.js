98f4217770f59340623bc6a3b358ec83074b9bce
Siarhei Ramanishkin <siarhei.ramonak@gmail.com>
2026-07-21 02:14:40 +0300

    task-8: fix formatting issues in tasks 3 and 4

--- task-4-cypress/cypress/e2e/product_overview.cy.js
+++ task-4-cypress/cypress/e2e/product_overview.cy.js
@@ -3,10 +3,10 @@ import HomePage from "../pages/home.page";
 const homePage = new HomePage();
 
 describe("product overview", () => {
-  it("should search for a product by name", () => {
-    homePage.open();
-    const term = "Pliers";
-    homePage.search(term);
-    homePage.searchCaption.should("include.text", term);
-  });
+	it("should search for a product by name", () => {
+		homePage.open();
+		const term = "Pliers";
+		homePage.search(term);
+		homePage.searchCaption.should("include.text", term);
+	});
 });
--- task-4-cypress/cypress/e2e/sign_in.cy.js
+++ task-4-cypress/cypress/e2e/sign_in.cy.js
@@ -5,33 +5,29 @@ import SignInPage from "../pages/sign_in.page";
 const signInPage = new SignInPage();
 
 describe("Sign in", () => {
-  beforeEach(() => {
-    signInPage.open();
-  });
+	beforeEach(() => {
+		signInPage.open();
+	});
 
-  it("should successfully sign in user with valid credentials", () => {
-    signInPage.login(users.validUser_1.email, users.validUser_1.password);
+	it("should successfully sign in user with valid credentials", () => {
+		signInPage.login(users.validUser_1.email, users.validUser_1.password);
 
-    cy.url().should("include", routes.account);
-  });
+		cy.url().should("include", routes.account);
+	});
 
-  it("fails to sign in user with missing credentials", () => {
-    signInPage.loginButton.click();
-    cy.url().should("include", routes.login);
+	it("fails to sign in user with missing credentials", () => {
+		signInPage.loginButton.click();
+		cy.url().should("include", routes.login);
 
-    signInPage.emailError
-      .should("be.visible")
-      .and("include.text", "Email is required");
+		signInPage.emailError.should("be.visible").and("include.text", "Email is required");
 
-    signInPage.passwordError
-      .should("be.visible")
-      .and("include.text", "Password is required");
-  });
-  it("lockout user after repeated failed sign in attempts", () => {
-    for (let i = 1; i <= 4; i++) {
-      signInPage.login(users.lockoutTest.email, users.lockoutTest.password);
-    }
+		signInPage.passwordError.should("be.visible").and("include.text", "Password is required");
+	});
+	it("lockout user after repeated failed sign in attempts", () => {
+		for (let i = 1; i <= 4; i++) {
+			signInPage.login(users.lockoutTest.email, users.lockoutTest.password);
+		}
 
-    signInPage.loginError.should("be.visible").and("include.text", "lock");
-  });
+		signInPage.loginError.should("be.visible").and("include.text", "lock");
+	});
 });

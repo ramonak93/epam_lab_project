319dec43c643099fe3195c29968758f5ee342cde
Siarhei Ramanishkin <siarhei.ramonak@gmail.com>
2026-07-21 01:36:56 +0300

    task-8: setup ling and prettier for task-3

--- task-3-playwright/tests/specs/product_overview.spec.js
+++ task-3-playwright/tests/specs/product_overview.spec.js
@@ -1,25 +1,18 @@
 import { test, expect } from "../fixtures/pages.fixture";
-import { SignInPage } from "../../pages/sign_in.page";
-import { HomePage } from "../../pages/home.page";
 
 test.describe("product overview", () => {
-  test("should navigate to product detail when card is clicked", async ({
-    page,
-    homePage,
-  }) => {
-    const productCardId = await homePage.getCardId(3);
-    await homePage.clickCard(productCardId);
+	test("should navigate to product detail when card is clicked", async ({ page, homePage }) => {
+		const productCardId = await homePage.getCardId(3);
+		await homePage.clickCard(productCardId);
 
-    await expect(page).toHaveURL(`/product/${productCardId}`);
-  });
-  test("should navigate between pages of products", async ({
-    page,
-    homePage,
-  }) => {
-    const page1Ids = await homePage.getAllCardIds();
-    await homePage.goToNextPage();
-    const page2Ids = await homePage.getAllCardIds();
+		await expect(page).toHaveURL(`/product/${productCardId}`);
+	});
 
-    expect(page2Ids).not.toEqual(page1Ids);
-  });
+	test("should navigate between pages of products", async ({ homePage }) => {
+		const page1Ids = await homePage.getAllCardIds();
+		await homePage.goToNextPage();
+		const page2Ids = await homePage.getAllCardIds();
+
+		expect(page2Ids).not.toEqual(page1Ids);
+	});
 });
--- task-3-playwright/tests/specs/sign_in.spec.js
+++ task-3-playwright/tests/specs/sign_in.spec.js
@@ -1,25 +1,16 @@
 import { test, expect } from "../fixtures/pages.fixture.js";
-import { SignInPage } from "../../pages/sign_in.page.js";
 import { routes, users } from "../data/index";
 
 test.describe("sign in", () => {
-  test("successfully signs in as admin with valid credentials", async ({
-    page,
-    signInPage,
-  }) => {
-    await signInPage.login(users.admin.email, users.admin.password);
+	test("successfully signs in as admin with valid credentials", async ({ page, signInPage }) => {
+		await signInPage.login(users.admin.email, users.admin.password);
 
-    await expect(page).toHaveURL(routes.adminDashboard);
-  });
-  test("fails to sign user in with invalid credentials", async ({
-    page,
-    signInPage,
-  }) => {
-    await signInPage.login(
-      users.invalidCredentials.email,
-      users.invalidCredentials.password,
-    );
+		await expect(page).toHaveURL(routes.adminDashboard);
+	});
 
-    await expect(page).toHaveURL(routes.login);
-  });
+	test("fails to sign user in with invalid credentials", async ({ page, signInPage }) => {
+		await signInPage.login(users.invalidCredentials.email, users.invalidCredentials.password);
+
+		await expect(page).toHaveURL(routes.login);
+	});
 });

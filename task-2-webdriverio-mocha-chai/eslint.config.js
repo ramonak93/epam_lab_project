eee5933a45b3d5e6e6737b48f77a4f6fce8e12c9
Siarhei Ramanishkin <siarhei.ramonak@gmail.com>
2026-07-21 02:07:59 +0300

    task-8: setup lint and prettier for task 4; minor esling.config changes in task-2 and task-3

--- task-2-webdriverio-mocha-chai/eslint.config.js
+++ task-2-webdriverio-mocha-chai/eslint.config.js
@@ -1,12 +1,21 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import mochaPlugin from "eslint-plugin-mocha";
 import { configs as wdioConfig } from "eslint-plugin-wdio";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
 	{
 		ignores: ["node_modules/", "allure-results/", "allure-report/", "reports/", "coverage/"],
+	},
+
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
 	},
 
 	js.configs.recommended,

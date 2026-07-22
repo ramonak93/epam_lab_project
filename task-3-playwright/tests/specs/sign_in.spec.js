01992349731bf75a7350aa540c66ac578adec4b9
Siarhei Ramanishkin <siarhei.ramonak@gmail.com>
2026-07-22 22:00:57 +0300

    task-8: again fix issues locally from a bad merge

--- task-3-playwright/tests/specs/sign_in.spec.js
+++ task-3-playwright/tests/specs/sign_in.spec.js
@@ -7,17 +7,10 @@ test.describe("Sign in", () => {
 
 		await expect(page).toHaveURL(routes.adminDashboard);
 	});
-
-	test("fails to sign user in with invalid credentials", async ({ page, signInPage }) => {
-		await signInPage.login(users.invalidCredentials.email, users.invalidCredentials.password);
-		await expect(page).toHaveURL(routes.adminDashboard);
-	});
-
 	test("fails to sign user in with invalid credentials", async ({ page, signInPage }) => {
 		await signInPage.login(users.invalidCredentials.email, users.invalidCredentials.password);
 
-		await expect(page).toHaveURL(routes.login);
-	});
 		await expect(page).toHaveURL(routes.login);
 	});
 });
+``;

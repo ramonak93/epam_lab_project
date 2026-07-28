8b6d5aabf5a0a0cfe08a52ee9530d894a008a0af
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 18:19:14 +0300

    update

--- task-4-cypress/cypress/pages/base.page.js
+++ task-4-cypress/cypress/pages/base.page.js
@@ -4,14 +4,12 @@ class BasePage {
 	}
 
 	open() {
-		cy.visit((`https://practicesoftwaretesting.com${this.url}`), 
-		// {
-			// headers: {
-			// 	"Accept" : "application/json, text/plain, */*",
-			// 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-			// }
-		// }
-	)
+		cy.visit((`https://practicesoftwaretesting.com${this.url}`), {
+			headers: {
+				"Accept" : "application/json, text/plain, */*",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:153.0) Gecko/20100101 Firefox/153.0"
+			}
+		})
 	}
 }
 

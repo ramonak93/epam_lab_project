bfcc211f1547311169d3f34264c5382a97715338
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 17:06:38 +0300

    update

--- task-4-cypress/cypress/pages/base.page.js
+++ task-4-cypress/cypress/pages/base.page.js
@@ -4,10 +4,10 @@ class BasePage {
 	}
 
 	open() {
-		cy.visit((this.url), {
+		cy.visit((`https://practicesoftwaretesting.com${this.url}`), {
 			headers: {
 				"Accept" : "application/json, text/plain, */*",
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 			}
 		})
 	}

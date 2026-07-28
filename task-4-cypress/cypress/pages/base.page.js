74a36d40686e7220db311cd2089152cc5fe7deda
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 18:12:02 +0300

    update

--- task-4-cypress/cypress/pages/base.page.js
+++ task-4-cypress/cypress/pages/base.page.js
@@ -4,12 +4,14 @@ class BasePage {
 	}
 
 	open() {
-		cy.visit((`https://practicesoftwaretesting.com${this.url}`), {
-			headers: {
-				"Accept" : "application/json, text/plain, */*",
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-			}
-		})
+		cy.visit((`https://practicesoftwaretesting.com${this.url}`), 
+		// {
+			// headers: {
+			// 	"Accept" : "application/json, text/plain, */*",
+			// 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+			// }
+		// }
+	)
 	}
 }
 

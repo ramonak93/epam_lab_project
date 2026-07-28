6c8b1c737c52dfc835b25d1dea1f866537c6d1f0
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 15:21:35 +0300

    update

--- task-2-webdriverio-mocha-chai/config/wdio.conf.js
+++ task-2-webdriverio-mocha-chai/config/wdio.conf.js
@@ -230,7 +230,7 @@ export const config = {
 	 * @param {boolean} result.passed    true if test has passed, otherwise false
 	 * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
 	 */
-	afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+	afterTest: async function (test, context, {  passed }) {
 		if (!passed) {
 			await fs.mkdirSync("./screenshots", { recursive: true });
 			const timestamp = new Date().toISOString().replace(/[:.]/g, "-");

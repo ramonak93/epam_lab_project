b6dd12c74d761ead07fdad9fb206602309882bdb
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 17:03:50 +0300

    user agent update

--- task-4-cypress/cypress.config.js
+++ task-4-cypress/cypress.config.js
@@ -7,8 +7,19 @@ export default defineConfig({
 	e2e: {
 		baseUrl: "https://practicesoftwaretesting.com",
 		screenshotOnRunFailure: true,
-		// setupNodeEvents(on, config) {
-		//   // implement node event listeners here
-		// },
+		setupNodeEvents(on) {
+			on("before:browser:launch", (browser, launchOptions) => {
+				if (browser.family === "chromium") {
+					launchOptions.args.push(
+						"--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+					);
+				}
+				if (browser.family === "firefox") {
+					launchOptions.preferences["general.useragent.override"] =
+						"Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0";
+				}
+				return launchOptions;
+			});
+		},
 	},
 });

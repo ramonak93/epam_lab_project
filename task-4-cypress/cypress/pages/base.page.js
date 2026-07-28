3672662d8fd11984d393b28f6f9cf3135909757c
kshukanova <kseniya_shukanava@epam.com>
2026-07-28 15:42:43 +0300

    fix for CY

--- task-4-cypress/cypress/pages/base.page.js
+++ task-4-cypress/cypress/pages/base.page.js
@@ -4,7 +4,11 @@ class BasePage {
 	}
 
 	open() {
-		cy.visit(this.url);
+		cy.visit((this.url), {
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+			}
+		})
 	}
 }
 
